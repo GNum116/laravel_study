e7b0dabb40b6da3e2c26189218298e1f67dd18e5
Num116 <num116g@gmail.com>
2017-08-31 16:14:43 +0800

    Finish user signup

---
+++ resources/lang/zh-CN/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'custom' => [
+        'email' => [
+            'required' => '邮箱 不能为空！'
+        ]
+    ]
+];
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,12 +6,16 @@
 
 <link rel="stylesheet" href="/css/app.css">
 </head>
+
 <body>
 @include('layouts._header')
 
 <div class="container">
+  <div class="col-md-offset-1 col-md-10">
+    @include('shared._message')
     @yield('content')
     @include('layouts._footer')
+  </div>
 </div>
 </body>
 </html>
