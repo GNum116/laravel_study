37d6ce442fd594b7bef0ad7fb61fe360dc5f116d
Num116 <num116g@gmail.com>
2017-09-04 17:20:01 +0800

    Finish user edit, update, index and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@
 */
 
 Route::get('/', 'StaticPagesController@home')->name('home');
-Route::get('/help', 'StaticPagesController@help')->name('help');
-Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::resource('users', 'UsersController');
@@ -21,3 +21,5 @@ Route::resource('users', 'UsersController');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('users/{uid}/edit', 'UsersController@edit')->name('users.edit');
