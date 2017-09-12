ebcab845bfce05933d0998489a54520fce527aa6
Num116 <num116g@gmail.com>
2017-09-12 10:17:43 +0800

    Add user statuses

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -3,20 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class StaticPagesController extends Controller
 {
     public function home()
 	{
 		#return '主页';
-		return view('static_pages/home');
+        if (Auth::check()) {
+            $feedItems = Auth::user()->feed()->paginate(10);
+        }
+
+		return view('static_pages/home', compact('feedItems'));
 	}
 
 	public function help()
 	{
 		#return '帮助页';
 		return view('static_pages/help');
-	} 
+	}
 
 	public function about()
 	{
